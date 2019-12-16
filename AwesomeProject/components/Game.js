d436b64ff50d89db65c13db66b1862ec377ada31
Teressa <tson1188@gmail.com>
2019-12-16 14:29:12 -0500

    added vending machine image

--- AwesomeProject/components/Game.js
+++ AwesomeProject/components/Game.js
@@ -5,17 +5,55 @@ import {
   ScrollView,
   Text,
   StyleSheet,
+  ImageBackground
 } from 'react-native';
 import { Icon } from 'react-native-elements'
 import { PacificoText } from '../components/StyledText';
 import TopBar from '../components/TopBar'
 
-export default class Candies extends Component {
+export default class Game extends Component {
   constructor (props){
     super(props)
     this.state = {
     }
   }
-
-
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Hi this is the game view.</Text>
+        <ImageBackground style={styles.image} source={require('../assets/images/vending.png')}>
+        </ImageBackground>
+      </View>
+    )
+  }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    // marginTop: 10,
+    // color: 'white'
+  },
+  // text:{
+  //   color:'white',
+  //   fontSize: 18,
+  //   textTransform: 'uppercase',
+  //   marginBottom: 15,
+  //   fontWeight: '800'
+  // },
+  // header: {
+  //   fontSize: 30,
+  //   textTransform: "uppercase",
+  //   marginBottom: 5,
+  //   marginTop: 5,
+  //   color: 'white',
+  //   fontWeight: '800'
+  // },
+  image: {
+    width: '97%',
+    height: '97%'
+}
+});
--- AwesomeProject/screens/GameScreen.js
+++ AwesomeProject/screens/GameScreen.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 import Game from '../components/Game'
 
 export default function GameScreen() {
   return (
+    <View>
     <Game />
+    </View>
   )
 }
 
 GameScreen.navigationOptions = {
-  title: 'Game',
+  title: 'Earn Points',
 };
